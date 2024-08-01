23fb1208ad376f220148ab92af0117308f2b5872
RychidM <richardsk2@gmail.com>
2024-08-01 17:56:48 +0000

    completed auth service

---
+++ lib/app/modules/home/state.dart
@@ -0,0 +1,5 @@
+class HomeState {
+  // final Rxn<User?> _faithUser = Rxn<User?>(null);
+  // User? get faithUser => _faithUser.value;
+  // set setFaithUser(User value) => _faithUser.value = value;
+}
