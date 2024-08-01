d5b85e2ae388f57eb916dff593d16faaa869c9db
RychidM <richardsk2@gmail.com>
2024-08-01 17:56:09 +0000

    completed auth service

--- lib/app/modules/auth/sign_in/widgets/sign_in_body.dart
+++ lib/app/modules/auth/sign_in/widgets/sign_in_body.dart
@@ -1,8 +1,10 @@
+import 'package:faith_fund/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 import 'package:rmdev_widgets/buttons/rm_button.dart';
+import 'package:rmdev_widgets/form_validator.dart';
 import 'package:rmdev_widgets/text_fields/rm_textfield.dart';
 
 import '../index.dart';
@@ -15,113 +17,128 @@ class SignInBody extends GetView<SignInController> {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 30.h),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Image.asset(
-            "assets/logos/logo_trans.png",
-            height: 170.h,
-          ),
-          Gap(40.h),
-          RmTextFormField(
-            fieldBorderRadius: 15,
-            contentPaddingLeft: 15,
-            contentPaddingTop: 15,
-            contentPaddingBottom: 15,
-            contentPaddingRight: 15,
-            hintText: "Email Address",
-            hintTextColor: const Color.fromARGB(255, 158, 158, 161),
-            textInputType: TextInputType.emailAddress,
-            textInputAction: TextInputAction.next,
-            onChanged: (value) {},
-          ),
-          Gap(20.h),
-          RmTextFormField(
-            obscureText: true,
-            textInputAction: TextInputAction.done,
-            fieldBorderRadius: 15,
-            contentPaddingLeft: 15,
-            contentPaddingTop: 15,
-            contentPaddingBottom: 15,
-            contentPaddingRight: 15,
-            hintText: "Password",
-            hintTextColor: const Color.fromARGB(255, 158, 158, 161),
-            onChanged: (value) {},
-          ),
-          Gap(40.h),
-          RmButton(
-            onTap: () {},
-            label: "Sign In",
-            color: const Color(0xff4091a5),
-          ),
-          Gap(20.h),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 120.w,
-                height: 1.h,
-                color: const Color(0xffe1e1e6),
-              ),
-              Gap(10.w),
-              Image.asset(
-                "assets/logos/launch_no_text.png",
-                height: 25.h,
-                color: const Color(0xffe1e1e6),
-              ),
-              Gap(10.w),
-              Text(
-                "OR",
-                style: context.textTheme.bodyMedium
-                    ?.copyWith(color: const Color.fromARGB(255, 158, 158, 161)),
-              ),
-              Gap(10.w),
-              Image.asset(
-                "assets/logos/launch_no_text.png",
-                height: 25.h,
-                color: const Color(0xffe1e1e6),
-              ),
-              Gap(10.w),
-              Container(
-                width: 120.w,
-                height: 1.h,
-                color: const Color(0xffe1e1e6),
-              ),
-            ],
-          ),
-          Gap(40.h),
-          RmButton(
-            onTap: () {},
-            color: Colors.white,
-            side: BorderSide(
-              width: 1.w,
-              color: const Color(0xffe1e1e6),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              "assets/logos/logo_trans.png",
+              height: 170.h,
             ),
-            child: SizedBox(
-              height: 60.h,
-              width: double.maxFinite,
-              child: Stack(
+            Gap(40.h),
+            Form(
+              key: controller.signInKey,
+              child: Column(
                 children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Image.asset(
-                      "assets/logos/google-logo.png",
-                      height: 25.h,
-                    ),
+                  RmTextFormField(
+                    controller: controller.emailController,
+                    fieldBorderRadius: 15,
+                    contentPaddingLeft: 15,
+                    contentPaddingTop: 15,
+                    contentPaddingBottom: 15,
+                    contentPaddingRight: 15,
+                    hintText: "Email Address",
+                    hintTextColor: const Color.fromARGB(255, 158, 158, 161),
+                    textInputType: TextInputType.emailAddress,
+                    textInputAction: TextInputAction.next,
+                    onChanged: (value) {},
+                    validator: (value) => RmFormValidator.requiredValidator.call(value),
                   ),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      "Continue with Google",
-                      style: context.textTheme.bodyMedium
-                          ?.copyWith(fontSize: 16.sp),
-                    ),
-                  )
+                  Gap(20.h),
+                  RmTextFormField(
+                    controller: controller.passwordController,
+                    obscureText: true,
+                    textInputAction: TextInputAction.done,
+                    fieldBorderRadius: 15,
+                    contentPaddingLeft: 15,
+                    contentPaddingTop: 15,
+                    contentPaddingBottom: 15,
+                    contentPaddingRight: 15,
+                    hintText: "Password",
+                    hintTextColor: const Color.fromARGB(255, 158, 158, 161),
+                    validator: (value) => RmFormValidator.requiredValidator.call(value),
+                  ),
                 ],
               ),
             ),
-          )
-        ],
+            Gap(40.h),
+            Obx(() {
+              return RmButton(
+                isLoading: controller.state.isLoading,
+                onTap: () =>controller.signInFaithUser(),
+                label: "Sign In",
+                color: const Color(0xff4091a5),
+              );
+            }),
+            Gap(20.h),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 120.w,
+                  height: 1.h,
+                  color: const Color(0xffe1e1e6),
+                ),
+                Gap(10.w),
+                Image.asset(
+                  "assets/logos/launch_no_text.png",
+                  height: 25.h,
+                  color: const Color(0xffe1e1e6),
+                ),
+                Gap(10.w),
+                Text(
+                  "OR",
+                  style: context.textTheme.bodyMedium
+                      ?.copyWith(color: const Color.fromARGB(255, 158, 158, 161)),
+                ),
+                Gap(10.w),
+                Image.asset(
+                  "assets/logos/launch_no_text.png",
+                  height: 25.h,
+                  color: const Color(0xffe1e1e6),
+                ),
+                Gap(10.w),
+                Container(
+                  width: 120.w,
+                  height: 1.h,
+                  color: const Color(0xffe1e1e6),
+                ),
+              ],
+            ),
+            Gap(40.h),
+            RmButton(
+              onTap: () {},
+              color: Colors.white,
+              side: BorderSide(
+                width: 1.w,
+                color: const Color(0xffe1e1e6),
+              ),
+              child: SizedBox(
+                height: 60.h,
+                width: double.maxFinite,
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Image.asset(
+                        "assets/logos/google-logo.png",
+                        height: 25.h,
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Continue with Google",
+                        style: context.textTheme.bodyMedium
+                            ?.copyWith(fontSize: 16.sp),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
